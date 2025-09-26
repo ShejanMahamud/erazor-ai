2a44c70700fcf0bd166c94f2f30f3fee6e7e4d54
ShejanMahamud <dev.shejanmahamud@gmail.com>
2025-09-26 18:03:21 +0600

    refactor: Remove unnecessary comment in BackgroundRemover component

--- src/components/BackgroundRemover.tsx
+++ src/components/BackgroundRemover.tsx
@@ -206,7 +206,7 @@ export function BackgroundRemover() {
                         </Button>
                     )}
                 </div>
-                {/* Upload Section */}
+
                 {!originalImage && (
                     <div className="flex flex-col items-center justify-center min-h-[400px] space-y-4">
                         <Suspense>
