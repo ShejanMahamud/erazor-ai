64f1ccb08ba110c176a5ba35f6b291c695c79b91
ShejanMahamud <dev.shejanmahamud@gmail.com>
2025-09-30 15:35:54 +0600

    feat: Refactor anonymous user ID handling to improve cookie management and readability

--- src/app/api/root/anon-user/route.ts
+++ src/app/api/root/anon-user/route.ts
@@ -4,10 +4,14 @@ import { v4 as uuidv4 } from 'uuid';
 
 
 export async function GET() {
+    const cookieStore = await cookies();
+    const existingAnonId = cookieStore.get('anon_id')?.value;
+    if (existingAnonId) {
+        return NextResponse.json({ anon_id: existingAnonId });
+    }
     const anonUserId = `anon-${uuidv4()}`;
+    const ONE_YEAR_IN_SECONDS = 60 * 60 * 24 * 365;
     //set in cookies for 365 days
-    const cookieStore = await cookies()
-    cookieStore.set('anon_id', anonUserId, { maxAge: 60 * 60 * 24 * 365, sameSite: 'lax', secure: true, httpOnly: false, });
-
+    cookieStore.set('anon_id', anonUserId, { maxAge: ONE_YEAR_IN_SECONDS, sameSite: 'lax', secure: true, httpOnly: false, });
     return NextResponse.json({ anon_id: anonUserId });
 }
