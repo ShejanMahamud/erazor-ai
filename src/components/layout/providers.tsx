a041a7a8f79d9a90fd11fdb6c38d76371eb7b7df
ShejanMahamud <dev.shejanmahamud@gmail.com>
2025-09-30 22:17:18 +0600

    feat: Refactor Providers and ClerkWrapper components for improved theme handling and structure

--- src/components/layout/providers.tsx
+++ src/components/layout/providers.tsx
@@ -1,31 +1,19 @@
-'use client';
-import { ClerkProvider } from '@clerk/nextjs';
-import { dark } from '@clerk/themes';
-import { useTheme } from 'next-themes';
-import React from 'react';
-import { ActiveThemeProvider } from '../active-theme';
+"use client";
+
+import React from "react";
+import { ActiveThemeProvider } from "../active-theme";
+import { ClerkWrapper } from "./clerk-wrapper";
 
 export default function Providers({
   activeThemeValue,
-  children
+  children,
 }: {
   activeThemeValue: string;
   children: React.ReactNode;
 }) {
-  // we need the resolvedTheme value to set the baseTheme for clerk based on the dark or light theme
-  const { resolvedTheme } = useTheme();
-
   return (
-    <>
-      <ActiveThemeProvider initialTheme={activeThemeValue}>
-        <ClerkProvider
-          appearance={{
-            baseTheme: resolvedTheme === 'dark' ? dark : undefined
-          }}
-        >
-          {children}
-        </ClerkProvider>
-      </ActiveThemeProvider>
-    </>
+    <ActiveThemeProvider initialTheme={activeThemeValue}>
+      <ClerkWrapper>{children}</ClerkWrapper>
+    </ActiveThemeProvider>
   );
 }
