ec0da87cce0417cd7d11b31aa22554391f8a11ae
ShejanMahamud <dev.shejanmahamud@gmail.com>
2025-10-04 19:13:59 +0600

    fix: refactor GET function to streamline response handling and improve cookie management

--- src/app/api/session/route.ts
+++ src/app/api/session/route.ts
@@ -2,7 +2,7 @@ import { auth, clerkClient } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 import { v4 as uuidv4 } from 'uuid';
 
-export async function GET(res: NextResponse) {
+export async function GET() {
     const { userId, sessionId } = await auth();
 
     if (userId && sessionId) {
@@ -13,41 +13,40 @@ export async function GET(res: NextResponse) {
             const expireDate = new Date(session.expireAt).getTime();
             const now = Date.now();
             const maxAge = Math.max(0, Math.floor((expireDate - now) / 1000));
-
+            const res = NextResponse.json({
+                user_id: userId,
+            });
             res.cookies.set('user_id', userId, {
                 maxAge,
                 sameSite: 'none',
                 secure: true,
                 httpOnly: true,
             });
-
-            return NextResponse.json({
-                user_id: userId,
-                cookie_expires_in: maxAge,
-            });
+            return res;
         } else {
-            // session invalid → clear user_id before anonymous handling
+            const res = NextResponse.json({ message: 'session expired' });
+
             res.cookies.delete('user_id');
+            return res;
         }
     } else {
-        // not logged in → clear user_id if it exists
+        const res = NextResponse.json({ message: 'no active session' });
         res.cookies.delete('user_id');
+
+        // handle anonymous users
+        const existingAnonId = res.cookies.get('anon_id')?.value;
+        if (existingAnonId) {
+            return NextResponse.json({ anon_id: existingAnonId });
+        }
+
+        const anonUserId = `anon-${uuidv4()}`;
+        res.cookies.set('anon_id', anonUserId, {
+            maxAge: 60 * 60 * 24 * 365,
+            sameSite: 'none',
+            secure: true,
+            httpOnly: true,
+        });
+
+        return NextResponse.json({ anon_id: anonUserId });
     }
-
-    // handle anonymous users
-    const existingAnonId = res.cookies.get('anon_id')?.value;
-    if (existingAnonId) {
-        return NextResponse.json({ anon_id: existingAnonId });
-    }
-
-    const anonUserId = `anon-${uuidv4()}`;
-    res.cookies.set('anon_id', anonUserId, {
-        maxAge: 60 * 60 * 24 * 365,
-        sameSite: 'none',
-        secure: true,
-        httpOnly: true,
-    });
-
-    return NextResponse.json({ anon_id: anonUserId });
-
 }
