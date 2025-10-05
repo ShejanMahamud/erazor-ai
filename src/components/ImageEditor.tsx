054d540f0d63a9500cf3c0ca1f8c416e50718984
ShejanMahamud <dev.shejanmahamud@gmail.com>
2025-10-05 17:28:02 +0600

    fix: some bugs and ui issues

--- src/components/ImageEditor.tsx
+++ src/components/ImageEditor.tsx
@@ -1,7 +1,5 @@
 "use client";
-import { useTheme } from 'next-themes';
 import dynamic from 'next/dist/shared/lib/dynamic';
-import { useEffect } from 'react';
 import { TABS, TOOLS } from 'react-filerobot-image-editor';
 const FilerobotImageEditor = dynamic(
     () => import("react-filerobot-image-editor"),
@@ -16,7 +14,6 @@ export function ImageEditor({
     handleClose: () => void;
     imageSource: string;
 }) {
-    const { theme } = useTheme()
     const handleSaveImage = (editedImageObject: any) => {
         const { canvasBase64 } = editedImageObject;
 
@@ -25,26 +22,10 @@ export function ImageEditor({
         const link = document.createElement("a");
         link.href = canvasBase64; // base64 image URL
         link.download = "erazor_edited-image.png";
+        document.body.appendChild(link);
         link.click();
+        document.body.removeChild(link);
     }
-
-    useEffect(() => {
-        const root = document.documentElement;
-        const computedStyle = getComputedStyle(root);
-
-        if (theme === 'dark') {
-            root.style.setProperty('--fil-image-editor-bg', computedStyle.getPropertyValue('--background').trim());
-            root.style.setProperty('--fil-toolbar-bg', computedStyle.getPropertyValue('--card').trim());
-            root.style.setProperty('--fil-toolbar-btn-bg', computedStyle.getPropertyValue('--secondary').trim());
-            root.style.setProperty('--fil-toolbar-btn-color', computedStyle.getPropertyValue('--foreground').trim());
-        } else {
-            root.style.setProperty('--fil-image-editor-bg', computedStyle.getPropertyValue('--background').trim());
-            root.style.setProperty('--fil-toolbar-bg', computedStyle.getPropertyValue('--muted').trim());
-            root.style.setProperty('--fil-toolbar-btn-bg', computedStyle.getPropertyValue('--accent').trim());
-            root.style.setProperty('--fil-toolbar-btn-color', computedStyle.getPropertyValue('--foreground').trim());
-        }
-    }, [theme]);
-
 
     return (
         <div
